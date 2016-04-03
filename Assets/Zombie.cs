dc620334356e87a21778d990e76f3f601d77da29
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2016-04-03 12:39:23 +0100

    Added auto reset on losing

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Zombie : MonoBehaviour {
+	public float zombieStep = 0.01f;
+	private Vector3 origin;
 
 	// Use this for initialization
 	void Start () {
-		gameObject.transform.LookAt(new Vector3(
-			0f,
-			transform.position.y,
-			0f
-		));
+		origin = new Vector3 (0f, transform.position.y, 0f);
+		gameObject.transform.LookAt(origin);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector3.MoveTowards(transform.position, new Vector3(0f, transform.position.y, 0f), 0.01f);
+		transform.position = Vector3.MoveTowards(transform.position, origin, zombieStep);
+
+		if (transform.position == origin) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
 	}
 }
